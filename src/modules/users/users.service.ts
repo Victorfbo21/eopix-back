4c5677ffa5d6d49fbd37ded95559c6fb1d9f2bf2
Victor Bentes <victorfbo@outlook.com>
2024-02-14 21:11:23 -0300

    add create user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import UsersRepository from "./users.repository";
 import { ICreateUserInterface } from "./interfaces/create-user.interface";
-
+import { AppResponse } from "src/application/AppResponse";
 
 @Injectable()
 export default class UsersServices {
@@ -12,6 +12,16 @@ export default class UsersServices {
     async createUser(user: ICreateUserInterface) {
         try {
             const verifyUserExists = await this._usersRepository.findOneBy({ email: user.email })
+            if (verifyUserExists) {
+                return new AppResponse({
+                    data: null,
+                    error: true,
+                    statusCode: 400,
+                    message: "Usuário ja Cadastrado"
+                })
+            }
+
+
         }
         catch (err) {
 
